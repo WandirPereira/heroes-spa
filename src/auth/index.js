c0de74d5d5a3c7da4052e99f6a32af27ff7c5e34
WandirPereira <wandir@gmail.com>
2024-02-01 01:38:17 -0300

    Criando o projeto

---
+++ src/auth/index.js
@@ -0,0 +1,3 @@
+export * from "./pages/";
+export * from "./componentes/";
+export * from "./hooks/";
---
+++ src/heroes/index.js
@@ -0,0 +1,4 @@
+export * from "./componentes/";
+export * from "./pages/";
+export * from "./hooks/";
+export * from "./helpers/";
